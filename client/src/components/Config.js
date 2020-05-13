d6cb945aea0f94634c7efabf7d4ae3d305a922f4
Choi_JiYoul <wldbf97@naver.com>
2020-05-13 15:03:20 +0900

    Casts

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = 'adbaa158e2f1a20eb2aefa7d769646f3';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
---
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,100 @@
+import React, {useEffect, useState} from 'react'
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
+import { set } from 'mongoose';
+import MainImage from '../LandingPage/Sections/MainImage'
+import MovieInfo from './Sections/MovieInfo'
+import GridCards from '../commons/GridCards'
+import {Row} from 'antd'
+
+
+function MovieDetail(props) {
+
+    let movieId = props.match.params.movieId
+    const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
+
+    useEffect(() => {
+
+        //크루원들
+        let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+
+        //무비 정보
+        let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}`
+        
+        fetch(endpointInfo)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovie(response)
+            })
+
+
+        fetch(endpointCrew)
+        .then(response => response.json())
+        .then(response => {
+            setCasts(response.cast)
+        })
+
+
+    }, [])
+
+    const toggleActorView = () =>{
+        setActorToggle(!ActorToggle)
+    }
+
+
+
+
+    return (
+        <div>
+            {/* Header */}
+
+            <MainImage
+                image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                title={Movie.original_title}
+                text={Movie.overview}
+            />
+
+
+            {/* Body */}
+            <div style={{width:'85%', margin:'1rem auto'}}>
+
+                {/* Movie Info */}
+
+                <MovieInfo 
+                    movie={Movie}
+                />
+
+
+
+                <br />
+                {/* Actors Grid */}
+
+                <div style={{display: 'flex', justifyContent:'center', margin:'2rem'}}>
+                    <button onClick={toggleActorView}> Toggle Actor View </button>
+                </div>
+
+
+            {ActorToggle &&
+                <Row gutter={[16,16]}>
+                    {Casts && Casts.map((cast, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards 
+                                image={cast.profile_path ?
+                                    `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                characterName={cast.name}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+            }
+
+
+
+            </div>
+        </div>
+    )
+}
+
+export default MovieDetail
